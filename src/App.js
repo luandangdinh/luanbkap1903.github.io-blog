8ab6366a83f7ab246bf83850d7389646e4c32941
Dang.Dinh.Luan <dang.dinh.luan@framgia.com>
2020-10-08 16:44:22 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faTrash, faPlus, faEdit, faTimes, faBars } from "@fortawesome/free-solid-svg-icons";
 import { faFacebook, faTwitter, faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 library.add(faTrash, faEdit, faPlus, faBars, faTimes, faFacebook, faTwitter, faInstagram, faLinkedin);
 
 export default class App extends Component {
